310165b1f97aa69c5c5ba8ba7e463bc4f87b255b
Lucas <llovizzio@gmail.com>
2025-09-28 17:56:32 -0300

    Agrega funcionalidad para alternar el estado de completado de las tareas en el componente Todo; actualizar el componente TodoItems para manejar el nuevo prop toggleComplete

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
@@ -30,6 +30,23 @@ const Todo = () => {
       return prevTodos.filter((todo) => todo.id !== id);
     });
   }
+
+  const toggleComplete = (id) => {
+    setTodoList((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isComplete: !todo.isComplete };
+        }
+        return todo;
+      });
+    });
+  }
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
+
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
         
@@ -53,7 +70,7 @@ const Todo = () => {
 
         <div>
           {todoList.map((item, index) => {
-            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} />
+            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />
           })}
         </div>
     </div>
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -3,12 +3,13 @@ import tick from '../assets/check_box.svg'
 import not_tick from '../assets/check_box_outline_blank.svg'
 import delete_icon from '../assets/delete.svg'
 
-const TodoItems = ({ text, id, isComplete, deleteTodo }) => {
+const TodoItems = ({ text, id, isComplete, deleteTodo, toggleComplete }) => {
+
   return (
     <div className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1 items-center cursor-pointer'>
-            <img src={tick} alt="" className='w-7 '/>
-            <p className='text-slate-700 ml-4 text-[17px]'> {text} </p>
+        <div onClick={() => toggleComplete(id)} className='flex flex-1 items-center cursor-pointer'>
+            <img src={isComplete ? tick : not_tick} alt="" className='w-7'/>
+            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-700 ${isComplete ? 'line-through' : ''}`}> {text} </p>
         </div>
 
     <img src={delete_icon} alt="" className='w-9 hover:bg-red-500 rounded-full p-1 cursor-pointer' onClick={() => deleteTodo(id)} />
