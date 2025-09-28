38c5851f587a1e20e76415b466aaf1f5ea6e3aec
Lucas <llovizzio@gmail.com>
2025-09-28 00:24:02 -0300

    first commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import todo_icon from '../assets/todo_icon.png'
+import TodoItems from './TodoItems'
+
+const Todo = () => {
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+        
+        {/* ----------- Title ----------- */}
+
+        <div className='flex items-center mt-7 gap-2'>
+            <img src={todo_icon} className='w-8'/>
+            <h1 className='text-3xl font-semibold'> Todo list </h1>
+        </div>
+
+        {/* ----------- Input ----------- */}
+
+        <div className='flex items-center my-7 bg-stone-200 rounded-full'>
+          <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' 
+          placeholder='Añade una nueva tarea'/>
+          <button className='border-none rounded-full bg-blue-800 w-32 h-14 text-white text-lg font-medium cursor-pointer'> 
+            Añadir </button>
+        </div>
+
+        {/* ----------- Items ----------- */}
+
+        <div>
+          <TodoItems/>
+        </div>
+    </div>
+  )
+}
+
+export default Todo
