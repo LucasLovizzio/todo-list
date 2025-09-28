ed4f34a2eefcb906ec763addc7bb5481283c8b01
Lucas <llovizzio@gmail.com>
2025-09-28 00:43:54 -0300

    Actualizar imágenes de íconos a formato SVG en el componente TodoItems

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import tick from '../assets/tick.png'
-import not_tick from '../assets/not_tick.png'
-import delete_icon from '../assets/delete.png'
+import tick from '../assets/check_box.svg'
+import not_tick from '../assets/check_box_outline_blank.svg'
+import delete_icon from '../assets/delete.svg'
 
 const TodoItems = () => {
   return (
     <div className='flex items-center my-3 gap-2'>
         <div className='flex flex-1 items-center cursor-pointer'>
-            <img src={tick} alt="" className='w-7'/>
+            <img src={tick} alt="" className='w-7 '/>
             <p className='text-slate-700 ml-4 text-[17px]'> Tarea de ejemplo </p>
         </div>
 
-    <img src={delete_icon} alt="" className='w-3.5 cursor-pointer'/>
+    <img src={delete_icon} alt="" className='w-9 hover:bg-red-500 rounded-full p-1 cursor-pointer'/>
 
     </div>
   )
